1bd6d30054ce95f95835e0d03f848bdcaf304ca4
thaolt267 <thaolt267@gmail.com>
2021-07-25 14:15:37 +0700

    Update ex4

--- src/week1/Exercise04.java
+++ src/week1/Exercise04.java
@@ -8,34 +8,28 @@ public class Exercise04 {
 //        output { 5,4,3,2,1 }
 
         int myArr[] = new int[]{1, 2, 3, 4, 5};
-        int[] newArr = new int[5];
-        int temp=0;
+        int[] newArr = new int[myArr.length];
+        int temp = 0;
 
-        for (int i = 0; i < myArr.length; i++) {
-            for (int j = i + 1; j < myArr.length; j++) {
-                if (myArr[j] > myArr[i]) {
-                    temp = myArr[i];
-                    myArr[i] = myArr[j];
-                    myArr[j] = temp;
-
-                }
-            }
+        // Dao nguoc mang cu
+        for (int i = 0; i < myArr.length/2; i++) {
+            temp = myArr[i];
+            myArr[i] = myArr[myArr.length - i - 1];
+            myArr[myArr.length - i - 1] = temp;
         }
         System.out.println("New array after reverting : ");
-        for(int i : myArr){
-            System.out.printf("%d ",i);
+        for (int i : myArr) {
+            System.out.printf("%d ", i);
         }
-        // Bo mang cu vo mang moi, chi revert ko so snah gi het
-//        for (int i = 0; i < myArr.length - 1; i++){
-//            for( int j = newArr.length - 1; j >= 1; j-- ){
-//                newArr[j] = myArr[i];
-//
-//            }
-//        }
-//        System.out.println("New array after reverting : ");
-//        for(int j : newArr){
-//            System.out.printf("%d ",j);
-//        }
+        
+        // Bo mang cu vo mang moi theo thu tụ nguoc lai mang cu
+        for (int i = 0; i < myArr.length; i++) {
+            newArr[myArr.length - i - 1] = myArr[i];
+        }
+        System.out.println("New array after reverting : ");
+        for (int i : newArr) {
+            System.out.printf("%d ", i);
+        }
 
     }
 }
